d4ee584da8ae6558b43a7157e1f53083c17e9947
scottzyang <scottzyang@gmail.com>
2023-05-06 22:55:57 -0500

    Models completed

--- src/models/family.js
+++ src/models/family.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const familySchema = new Schema({
   name: { type: String, required: true },
-  scientific_name: { type: String, required: true }
+  scientificName: { type: String, required: true }
 }, { timestamps: true });
 
 const Family = model("Family", familySchema)
--- src/models/fish.js
+++ src/models/fish.js
@@ -3,7 +3,7 @@ const Populate = require('../../util/autopopulate');
 
 const fishSchema = new Schema({
   name: { type: String, required: true },
-  scientific_name: { type: String, required: true },
+  scientificName: { type: String, required: true },
   image: {type: String, required: true },
   family: { type: Schema.Types.ObjectId, ref: "Family", required: true },
   environment: [{ type: Schema.Types.ObjectId, ref: "Environment", required: true }],
@@ -13,7 +13,9 @@ const fishSchema = new Schema({
 // populate associated fields prior to searches
 fishSchema
   .pre('findOne', Populate('family'))
-  .pre('find', Populate('environment'));
+  .pre('findOne', Populate('environment'))
+  .pre('findById', Populate('family'))
+  .pre('findById', Populate('environment'));
 
 const Fish = model("Fish", fishSchema)
 
