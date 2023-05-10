0b55f09ffca73a47f7c6dca4321d18e9035c9689
scottzyang <scottzyang@gmail.com>
2023-05-10 16:50:32 -0500

    Complete user test

--- test/user.js
+++ test/user.js
@@ -3,6 +3,7 @@ const chaiHttp = require('chai-http');
 const { describe, it, after } = require('mocha');
 const app = require('../src/server');
 
+const expect = chai.expect
 const should = chai.should();
 
 chai.use(chaiHttp);
@@ -13,11 +14,15 @@ const agent = chai.request.agent(app);
 const User = require('../src/models/user');
 
 describe('User', function () {
-  const newUser = {
-    username: 'testone',
-    password: 'password',
-    email: 'test@email.com'
-  }
+  beforeEach( async () => {
+    const user = {
+      username: "usertest",
+      password: "passwordtest",
+      email: "test@email.com"
+    }
+    const apiKey = await chai.request(app).post('/user/register').send(user);
+    console.log(apiKey.apiKey)
+  })
 
   it('should not be able to retrieve token/apiKey if they have not registered', function (done) {
     agent.post('/user/auth', { email: 'wrong@example.com', password: 'nope' }).end(function (err, res) {
@@ -28,13 +33,21 @@ describe('User', function () {
 
   // register
   it('should be able to register', function (done) {
+    const newUser = {
+      username: 'testone',
+      password: 'password',
+      email: 'test@email.com'
+    }
+
     User.findOneAndRemove({ username: 'testone' })
       .then(() => {
         agent
           .post('/user/register')
           .send(newUser)
           .end(function (err, res) {
-            res.should.have.status(200);
+            res.should.have.status(200)
+            expect(res.body.apiKey)
+            console.log(res.body.apiKey)
             done();
           });
       })
@@ -47,7 +60,7 @@ describe('User', function () {
   it('should be able to login', function (done) {
     agent
       .post('/user/auth')
-      .send({ username: 'testone', password: 'password', email: 'test@email.com' })
+      .send({ username: 'usertest', password: 'passwordtest', email: 'test@email.com' })
       .end(function (err, res) {
         res.should.have.status(200);
         done();
